c3e934f7b95c1a8921292227d3acc93a2edc979f
Ryan Keegan <rkeega2@wgu.edu>
2025-07-11 01:20:10 -0400

    Update multiply and subtract calc button text to proper Unicode characters.

--- lib/data/button_values.dart
+++ lib/data/button_values.dart
@@ -6,11 +6,11 @@ class Btn {
   static const String n7 = '7';
   static const String n8 = '8';
   static const String n9 = '9';
-  static const String multiply = 'x';
+  static const String multiply = '\u2A09';
   static const String n4 = '4';
   static const String n5 = '5';
   static const String n6 = '6';
-  static const String subtract = '-';
+  static const String subtract = '\u2212';
   static const String n1 = '1';
   static const String n2 = '2';
   static const String n3 = '3';
